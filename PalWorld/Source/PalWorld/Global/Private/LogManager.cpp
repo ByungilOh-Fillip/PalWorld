6b0e0827c363f5ba84ec7a383ed91aece8313f4d
Fillip <103233073+ByungilOh-Fillip@users.noreply.github.com>
2026-06-23 16:09:35 +0900

    Setting LogManager
    - 로그 매니저 세팅

---
+++ PalWorld/Source/PalWorld/Global/Private/LogManager.cpp
@@ -0,0 +1,2 @@
+﻿#include "C:\Projects\PalWorld\PalWorld\PalWorld\Intermediate\Build\Win64\x64\PalWorldEditor\Development\UnrealEd\SharedPCH.UnrealEd.Project.ValApi.ValExpApi.Cpp20.h"
+#include "LogManager.h"
